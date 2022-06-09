a4ca2e97fdee3b3e99f1ccc03d496c9b3ad75fac
ahmadmajdi96 <44318068+ahmadmajdi96@users.noreply.github.com>
2022-06-09 15:09:09 +0300

    Add files via upload

---
+++ Solution1.py
@@ -0,0 +1,49 @@
+from stl import mesh 
+import requests
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot
+import math
+
+"""
+
+--- In case of downloading file from url ands saving it locally --
+
+#Storing url in a variable 
+url = "https://nervous-mouse.s3.eu-central-1.amazonaws.com/raw_039.stl"
+
+# Sending GET request and saving the response
+requestedFile = requests.get(url, allow_redirects=True)
+
+# Creating stl file from the content of the response
+open('raw_039.stl', 'wb').write(requestedFile.content)
+
+"""
+try: 
+    # Opening stl file 'raw_039.stl' using mesh
+    retrievedModel = mesh.Mesh.from_file('raw_039.stl')
+
+    # Rotating model 180 degrees around the y-axis
+    retrievedModel.rotate([0, 1, 0], math.radians(180))
+
+    # Saving model as 'raw_039.stl'
+    retrievedModel.save("raw_039_rotated.stl")
+    
+    RotatedModel = mesh.Mesh.from_file('raw_039_rotated.stl')
+
+
+    # Creating plot
+    figure = pyplot.figure()
+    axes = mplot3d.Axes3D(figure)
+    
+    # Adding 3d model to the plot
+    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(RotatedModel.vectors))
+    
+    # Scaling 3d model to plot
+    scale = RotatedModel.points.flatten()
+    axes.auto_scale_xyz(scale-10, scale+10, scale)
+    
+    # Opening the plot
+    pyplot.show()
+    
+except Exception as e: 
+    print(e)
---
+++ Solution2.py
@@ -0,0 +1,48 @@
+from stl import mesh 
+import requests
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot
+import math
+
+
+
+class ModelRotation:
+    def __init__(self, fileName):
+        self.fileName = fileName
+        self.rotatedName = "raw_039_rotated.stl"
+        self.openfile()
+        
+    def openfile(self):
+        # Opening stl file 'raw_039.stl' using mesh
+        self.retrievedModel = mesh.Mesh.from_file(self.fileName)
+        
+        # Rotating model 180 degrees around the y-axis
+        self.retrievedModel.rotate([0, 1, 0], math.radians(180))
+        
+        # Saving model as 'raw_039.stl'
+        self.retrievedModel.save("raw_039_rotated.stl")
+        
+        # Opening rotated model
+        self.rotatedModel = mesh.Mesh.from_file(self.rotatedName)
+        
+        self.plotmodel()
+        
+        
+    def plotmodel(self):
+        
+        #Creating plot
+        figure = pyplot.figure()
+        axes = mplot3d.Axes3D(figure)
+        
+        # Adding 3d model to the plot
+        axes.add_collection3d(mplot3d.art3d.Poly3DCollection(self.rotatedModel.vectors))
+        
+        # Scaling 3d model to plot
+        scale = self.rotatedModel.points.flatten()
+        axes.auto_scale_xyz(scale-10, scale+10, scale)
+        
+        # Showing the plot
+        pyplot.show()
+
+        
+ModelRotation('raw_039.stl')
